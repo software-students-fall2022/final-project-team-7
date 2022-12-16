734bd469d00cc66ed4ec3bdf52683cea08edb3b1
Yvonne511 <yw4142@nyu.edu>
2022-12-15 22:37:58 -0500

    OpenAI003

--- ml-client/converOpenAI.py
+++ ml-client/converOpenAI.py
@@ -8,7 +8,7 @@ openai.api_key = OPENAI_API_KEY
 
 def openAI(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         temperature=0.4,
         max_tokens=150,
