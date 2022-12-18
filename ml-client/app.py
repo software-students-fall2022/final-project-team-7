0ee4d9bb92e62fd2cbc5bb333669022de49f4445
Yvonne511 <yw4142@nyu.edu>
2022-12-17 23:30:32 -0500

    Router Param

--- ml-client/app.py
+++ ml-client/app.py
@@ -1,6 +1,7 @@
 from converOpenAI import openAI
 from processSpeech import transcribe
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -8,13 +9,13 @@ app = Flask(__name__)
 def containerSetUp():
     return 'Contianer is set up!'
 
-@app.route('/transcribe')
-def transcribe(fileName):
-    return transcribe(fileName)
+@app.route('/transcribe/<filename>')
+def transcriber(filename):
+    return transcribe(str(filename))
 
-@app.route('/openAI')
-def openAI(conversation):
-    return openAI(conversation)
+@app.route('/openAI/<prompt>')
+def openAIResponse(prompt):
+    return openAI(str(prompt))
 
 ## app.run(host=HOSTNAME, port=PORT, debug=True)
 if __name__ == "__main__":
