a2106b3a5b82954274b54de55462dece97a7ceef
WZ <midas0231@gmail.com>
2022-12-19 00:13:22 -0500

    Update test

--- web-client/tests/app_test.py
+++ web-client/tests/app_test.py
@@ -2,58 +2,61 @@ import sys
 sys.path.append('.')
 print(sys.path)
 
-import os
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 import pytest
 import pytest_flask
 import pymongo
 import mongomock
+import os
 
 
-def test_base_template():
+@pytest.fixture(scope='session')
+def client():
+    with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['username'] = 'test'
+            session['user_id'] = '639fe8c38d2fcfefcd49fa53'
+        yield client
+
+
+def test_base_template(client):
     # Test login route
-    client = app.test_client()
     url = '/'
     response = client.get(url)
-    assert response.status_code == 302
+    assert response.status_code == 200
 
 
-def test_error_template():
+def test_error_template(client):
     # Test a route that does not exist
-    client = app.test_client()
     url = '/errornotexist'
     response = client.get(url)
     assert response.status_code == 404
 
 
-def test_register_template():
+def test_register_template(client):
     # Test register route
-    client = app.test_client()
     url = '/register'
     response = client.get(url)
     assert response.status_code == 200
 
 
-def test_login_template():
-    # Test login route
-    client = app.test_client()
-    url = '/login'
+def test_home_template(client):
+    # Test home route
+    url = '/home'
     response = client.get(url)
     assert response.status_code == 200
 
 
-def test_history_template():
+def test_history_template(client):
     # Test history route
-    client = app.test_client()
     url = '/history'
     response = client.get(url)
-    assert response.status_code == 308
+    assert response.status_code == 200
 
 
-def test_history_range_template():
+def test_history_range_template(client):
     # Test history route
-    client = app.test_client()
     url = '/history/all'
     response = client.get(url)
     assert response.status_code == 200
@@ -67,17 +70,22 @@ def test_history_range_template():
     assert response.status_code == 200
 
 
-def test_profile_template():
+def test_profile_template(client):
     # Test profile route
-    client = app.test_client()
     url = '/profile'
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == 200
 
 
-def test_edit_template():
+def test_edit_template(client):
     # Test edit route
-    client = app.test_client()
     url = '/edit'
     response = client.get(url)
-    assert response.status_code == 404
+    assert response.status_code == 200
+
+
+def test_chatroom_template(client):
+    # Test edit route
+    url = '/chatroom'
+    response = client.get(url)
+    assert response.status_code == 200
